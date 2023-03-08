34044a3b8aebfbdca7073640dc53570f09ba313f
17043003 <35760051+17043003@users.noreply.github.com>
2023-03-09 07:24:57 +0900

    select fann and fu with spinner.

--- app/src/main/java/com/ishzk/android/majancalculator/ui/CalculatorViewModel.kt
+++ app/src/main/java/com/ishzk/android/majancalculator/ui/CalculatorViewModel.kt
@@ -8,21 +8,24 @@ import com.ishzk.android.majancalculator.domain.FannFu
 import com.ishzk.android.majancalculator.domain.WinnerParent
 
 class CalculatorViewModel: ViewModel() {
-    private val _fann = MutableLiveData<Int>(1)
-    val fann: LiveData<Int> = _fann
+    val fann = MutableLiveData<Int>(0)
+    val fanns = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
 
-    private val _fu = MutableLiveData<Int>(20)
-    val fu: LiveData<Int> = _fu
+    val fu = MutableLiveData<Int>(0)
+    val fus = listOf(20, 25) + (30..110 step 10)
 
     private val _calculateResult = MutableLiveData(0)
     val calculateResult: LiveData<Int> = _calculateResult
 
-    val fanns = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
-    val fus = listOf(20, 25) + (30..110 step 10)
+    private fun getFannFu(): FannFu{
+        val fann = fanns[fann.value ?: 0]
+        val fu = fus[fu.value ?: 0]
+        return FannFu(fann, fu)
+    }
 
     fun onClickResultButton(){
         val winner = WinnerParent()
-        val tsumoPoints = winner.receiveTsumoPoints(FannFu(fann.value ?: 1, fu.value ?: 30))
+        val tsumoPoints = winner.receiveTsumoPoints(getFannFu())
         _calculateResult.postValue(tsumoPoints.childPayment)
         Log.d(TAG, "${fann.value}-${fu.value} = ${tsumoPoints.childPayment}")
     }
