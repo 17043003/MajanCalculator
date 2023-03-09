528260f661aa90b549ffcb920d375c6562b0d778
17043003 <35760051+17043003@users.noreply.github.com>
2023-03-10 07:05:11 +0900

    add: show calculate result as string.
    - calculate by tsumo, parent, fann and fu.

--- app/src/main/java/com/ishzk/android/majancalculator/ui/CalculatorViewModel.kt
+++ app/src/main/java/com/ishzk/android/majancalculator/ui/CalculatorViewModel.kt
@@ -4,7 +4,9 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ishzk.android.majancalculator.domain.CalculateResult
 import com.ishzk.android.majancalculator.domain.FannFu
+import com.ishzk.android.majancalculator.domain.WinnerChild
 import com.ishzk.android.majancalculator.domain.WinnerParent
 
 class CalculatorViewModel: ViewModel() {
@@ -14,8 +16,11 @@ class CalculatorViewModel: ViewModel() {
     val fu = MutableLiveData<Int>(0)
     val fus = listOf(20, 25) + (30..110 step 10)
 
-    private val _calculateResult = MutableLiveData(0)
-    val calculateResult: LiveData<Int> = _calculateResult
+    val isTsumo = MutableLiveData(false)
+    val isParent = MutableLiveData(false)
+
+    private val _calculateResult = MutableLiveData("")
+    val calculateResult: LiveData<String> = _calculateResult
 
     private fun getFannFu(): FannFu{
         val fann = fanns[fann.value ?: 0]
@@ -24,10 +29,11 @@ class CalculatorViewModel: ViewModel() {
     }
 
     fun onClickResultButton(){
-        val winner = WinnerParent()
-        val tsumoPoints = winner.receiveTsumoPoints(getFannFu())
-        _calculateResult.postValue(tsumoPoints.childPayment)
-        Log.d(TAG, "${fann.value}-${fu.value} = ${tsumoPoints.childPayment}")
+        val winner = if(isParent.value == true) WinnerParent() else WinnerChild()
+        val result = CalculateResult(winner, isTsumo.value ?: false).pointString(getFannFu())
+
+        _calculateResult.postValue(result)
+        Log.d(TAG, "(tsumo:${isTsumo.value}, parent:${isParent.value}) ${fann.value}-${fu.value} = $result")
     }
 
     companion object{
