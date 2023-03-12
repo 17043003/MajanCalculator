7d065833a26c194f40d63b1e5b0453ca296e7659
17043003 <35760051+17043003@users.noreply.github.com>
2023-03-13 01:03:32 +0900

    add item on menu to WaitHandFragment.

---
+++ app/src/main/java/com/ishzk/android/majancalculator/ui/WaitHandFragment.kt
@@ -0,0 +1,24 @@
+package com.ishzk.android.majancalculator.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.ishzk.android.majancalculator.databinding.FragmentWaitHandBinding
+
+class WaitHandFragment : Fragment() {
+    private val viewModel: WaitHandViewModel by viewModels()
+    private var _binding: FragmentWaitHandBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWaitHandBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+}
