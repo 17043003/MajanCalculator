4dbb5e2caca46a0afa666c8298291bbc0eb07ef3
17043003 <35760051+17043003@users.noreply.github.com>
2023-03-12 20:17:43 +0900

    add navigation drawer to fragment transition.

--- app/src/main/java/com/ishzk/android/majancalculator/MainActivity.kt
+++ app/src/main/java/com/ishzk/android/majancalculator/MainActivity.kt
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.ishzk.android.majancalculator.databinding.ActivityMainBinding
 import com.ishzk.android.majancalculator.domain.PointRequestData
 import com.ishzk.android.majancalculator.domain.PointViewModel
 import com.ishzk.android.majancalculator.domain.TileKinds
@@ -14,12 +20,18 @@ import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     private val viewModel: PointViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.mainToolbar)
+        setDrawerMenu()
 
         lifecycleScope.launchWhenStarted {
             withContext(Dispatchers.IO) {
@@ -35,6 +47,19 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    // Setting navigation drawer to fragment transition.
+    private fun setDrawerMenu(){
+        val toggle = ActionBarDrawerToggle(
+            this, binding.root, binding.mainToolbar, R.string.app_name, R.string.app_name)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navController = (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment).navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.root)
+        binding.mainNavView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     companion object {
