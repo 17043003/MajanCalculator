dac14c27f02737105df334cda07af7e12bffcbb8
17043003 <35760051+17043003@users.noreply.github.com>
2023-04-12 07:15:12 +0900

    add: won hand class.

---
+++ app/src/test/java/com/ishzk/android/majancalculator/handpoint/WonHandShould.kt
@@ -0,0 +1,35 @@
+package com.ishzk.android.majancalculator.handpoint
+
+import com.ishzk.android.majancalculator.domain.handpoint.CloseTiles
+import com.ishzk.android.majancalculator.domain.handpoint.OpenTiles
+import com.ishzk.android.majancalculator.domain.handpoint.Tile
+import com.ishzk.android.majancalculator.domain.handpoint.WonHand
+import org.junit.Test
+
+class WonHandShould {
+    @Test
+    fun won_hand_is_valid(){
+        validWonHand()
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException::class)
+    fun won_hand_in_invalid(){
+        invalidWonHand()
+    }
+
+    private fun validWonHand(): WonHand {
+        val tiles = (1..9).map{ Tile("s", it) } + (1..4).map { Tile("m", it) }
+        val closeTiles = CloseTiles(tiles)
+        val openTiles = OpenTiles()
+        val winTile = Tile("m", 1)
+        return WonHand(closeTiles, openTiles, winTile)
+    }
+
+    private fun invalidWonHand(): WonHand {
+        val tiles = (1..9).map{ Tile("s", it) } + (1..4).map { Tile("m", it) } + Tile("s", 5)
+        val closeTiles = CloseTiles(tiles)
+        val openTiles = OpenTiles()
+        val winTile = Tile("m", 1)
+        return WonHand(closeTiles, openTiles, winTile)
+    }
+}
